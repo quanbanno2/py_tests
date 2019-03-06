b4d842f8accc02b323322c0dc235b7ea1ad14390
quanbanno2 <f821336459@163.com>
2019-03-06 16:49:21 +0800

    py_tests

--- py_tests.py
+++ py_tests.py
@@ -1,28 +1,53 @@
-import unittest, time
-from selenium import webdriver
+import unittest
 
 
-class TestClass(unittest.TestCase):
+def insertsort(arr):
+    for i in range(1, len(arr)):
+        # 设置当前值前一个元素的标识
+        j = i - 1
+        # 如果当前值小于前一个元素,则将当前值作为一个临时变量存储,
+        # 将前一个元素后移一位
+        # [5,6,4,3,2,1]
+        if (arr[i] < arr[j]):
 
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.base_url = "http://www.testclass.net"
+            # [5, ,6,3,2,1,]  j=1  将小的数放到缓存中
+            temp = arr[i]
+            arr[i] = arr[j]
 
-    def tearDown(self):
-        time.sleep(2)
-        self.driver.quit()
+            # 继续往前寻找,如果有比临时变量大的数字,则后移一位,
+            # 直到找到比临时变量小的元素或者达到列表第一个元素
 
-    def test_case(self):
-        self.driver.get(self.base_url)
-        search_input = self.driver.find_element_by_name("q")
-        search_input.send_keys("selenium")
-        search_input.submit()
+            # [5, ,6,3,2,1,]   j=0  arr[j]=5
+            j = j - 1
 
-    def test_case2(self):
-        self.driver.get(self.base_url)
-        search_input = self.driver.find_element_by_name("q")
-        search_input.send_keys("jenkins")
-        search_input.submit()
+            # 条件成立
+            while j >= 0 and arr[j] > temp:
+                # arr[1] = arr[0]
+                # j= -1 时跳出循环
+                arr[j + 1] = arr[j]
+                j = j - 1
+
+            # 将临时变量赋值给合适位置
+            # arr[-1+1] = 4
+            arr[j + 1] = temp
+
+
+class test_sort(unittest.TestCase):
+    def test_insertsort(self):
+        data_1 = [5, 6, 4, 3, 2, 1]
+        data_2 = [1, 3, 4, 5, 2]
+
+        copy_date1 = data_1
+        copy_date2 = data_2
+
+        insertsort(data_1)
+        insertsort(data_2)
+
+        copy_date1.sort()
+        copy_date2.sort()
+
+        self.assertEqual(copy_date1, data_1)
+        self.assertEqual(copy_date2, data_2)
 
 
 if __name__ == '__main__':
